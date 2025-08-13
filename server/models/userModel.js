cffb3203d7eb78870314ac015c8effca02930b27
yamuna <157932558+yamuna130@users.noreply.github.com>
2025-08-13 13:20:19 +0530

    Update userModel.js

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -2,27 +2,11 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
+    username: { type: String, required: true, unique: true, trim: true },
+    email: { type: String, required: true, unique: true, lowercase: true },
+    password: { type: String, required: true }
   },
   { timestamps: true }
 );
 
-const User = mongoose.model("User", userSchema);
-
-export default User;
-
+export default mongoose.model("User", userSchema);
